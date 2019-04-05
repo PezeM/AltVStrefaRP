41e69673956cf91f1aed0c62790d3fd685bf908a
PezeM <pezemos@gmail.com>
2019-04-05 10:52:44 +0200

    Dodano zapisywanie transakcji do bazy danych

--- AltVStrefaRPServer/Services/Money/MoneyService.cs
+++ AltVStrefaRPServer/Services/Money/MoneyService.cs
@@ -1,4 +1,5 @@
-﻿using AltVStrefaRPServer.Database;
+﻿using System.Threading.Tasks;
+using AltVStrefaRPServer.Database;
 using AltVStrefaRPServer.Models;
 
 namespace AltVStrefaRPServer.Services.Money
@@ -50,7 +51,55 @@ namespace AltVStrefaRPServer.Services.Money
             if (source.Money < amount) return false;
             source.Money -= amount;
             receiver.Money += amount;
+            receiver.Player.SetSyncedMetaData("money", receiver.Money);
+            source.Player.SetSyncedMetaData("money", source.Money);
+            LogMoneyTransaction(source.Id, receiver.Id, TransactionType.PlayerToPlayer, amount);
             return true;
+        }
+
+        /// <summary>
+        /// Transfers money from one character to another
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="receiver"></param>
+        /// <param name="amount"></param>
+        /// <returns>True if source character had enough money to transfer</returns>
+        public async Task<bool> TransferMoneyToAnotherPlayerAsync(Character source, Character receiver, float amount)
+        {
+            if (source.Money < amount) return false;
+            source.Money -= amount;
+            receiver.Money += amount;
+            receiver.Player.SetSyncedMetaData("money", receiver.Money);
+            source.Player.SetSyncedMetaData("money", source.Money);
+            await LogMoneyTransactionAsync(source.Id, receiver.Id, TransactionType.PlayerToPlayer, amount).ConfigureAwait(false);
+            return true;
+        }
+
+
+        /// <summary>
+        /// Saves transaction information to database
+        /// </summary>
+        /// <param name="source">Source of the transaction</param>
+        /// <param name="receiver">Receiver of the transactions</param>
+        /// <param name="type">Type of the transaction <see cref="TransactionType"/></param>
+        /// <param name="amount">Amount of transaction</param>
+        private void LogMoneyTransaction(int source, int receiver, TransactionType type, float amount)
+        {
+            _serverContext.MoneyTransactions.Add(new MoneyTransaction(source, receiver, type, amount));
+            _serverContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Saves transaction information to database
+        /// </summary>
+        /// <param name="source">Source of the transaction</param>
+        /// <param name="receiver">Receiver of the transactions</param>
+        /// <param name="type">Type of the transaction <see cref="TransactionType"/></param>
+        /// <param name="amount">Amount of transaction</param>
+        private async Task LogMoneyTransactionAsync(int source, int receiver, TransactionType type, float amount)
+        {
+            await _serverContext.MoneyTransactions.AddAsync(new MoneyTransaction(source, receiver, type, amount)).ConfigureAwait(false);
+            await _serverContext.SaveChangesAsync().ConfigureAwait(false);
         }
     }
 }
