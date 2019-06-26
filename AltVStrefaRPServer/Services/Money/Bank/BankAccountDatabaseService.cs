5622ce85ef2f3b553085aaedaa838281e698ad81
PezeM <pezemos@gmail.com>
2019-06-26 22:48:18 +0200

    Zaaktualizowano BankAccountDatabaseService
    Zmieniono BankHandler aby uzywal BankACcountDatabaseService

--- AltVStrefaRPServer/Services/Money/Bank/BankAccountDatabaseService.cs
+++ AltVStrefaRPServer/Services/Money/Bank/BankAccountDatabaseService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using AltVStrefaRPServer.Database;
 using AltVStrefaRPServer.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AltVStrefaRPServer.Services.Money.Bank
 {
@@ -19,6 +22,26 @@ namespace AltVStrefaRPServer.Services.Money.Bank
             using (var context = _factory.Invoke())
             {
                 return context.BankAccounts;
+            }
+        }
+
+        public async Task AddNewBankAccount(Character character)
+        {
+            using (var context = _factory.Invoke())
+            {
+                await context.BankAccounts.AddAsync(character.BankAccount);
+                context.Characters.Update(character);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<MoneyTransaction>> GetTransferHistory(Character character, int numberOfLastTransactions = 50)
+        {
+            using (var context = _factory.Invoke())
+            {
+                return await context.MoneyTransactions.AsNoTracking()
+                    .Where(t => t.Receiver == character.GetFullName() || t.Source == character.GetFullName())
+                    .Take(numberOfLastTransactions).ToListAsync();
             }
         }
     }
--- AltVStrefaRPServer/Services/Money/Bank/IBankAccountDatabaseService.cs
+++ AltVStrefaRPServer/Services/Money/Bank/IBankAccountDatabaseService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using AltVStrefaRPServer.Models;
 
 namespace AltVStrefaRPServer.Services.Money.Bank
@@ -6,5 +7,7 @@ namespace AltVStrefaRPServer.Services.Money.Bank
     public interface IBankAccountDatabaseService
     {
         IEnumerable<BankAccount> GetAllBankAccounts();
+        Task AddNewBankAccount(Character character);
+        Task<List<MoneyTransaction>> GetTransferHistory(Character character, int numberOfLastTransactions = 50);
     }
 }
