317fe145b0854c78e1e83600fd66db4eb5a09649
PezeM <pezemos@gmail.com>
2019-07-09 15:34:14 +0200

    Dodano wiecej testowych przedmiotow do item factory

--- AltVStrefaRPServer/Models/Inventory/Items/ItemType.cs
+++ AltVStrefaRPServer/Models/Inventory/Items/ItemType.cs
@@ -3,6 +3,8 @@
     public enum ItemType
     {
         Weapon_CombatPistol = 100,
+        Weapon_Pistol,
+        Weapon_PumpShotgun,
         FoodItem_Burger = 1000,
         FoodItem_Water,
         FoodItem_Sprunk,
--- AltVStrefaRPServer/Services/Inventory/ItemFactory.cs
+++ AltVStrefaRPServer/Services/Inventory/ItemFactory.cs
@@ -1,11 +1,12 @@
-﻿using AltVStrefaRPServer.Models.Inventory;
+﻿using AltV.Net.Enums;
+using AltVStrefaRPServer.Models.Inventory;
 using AltVStrefaRPServer.Models.Inventory.Items;
 
 namespace AltVStrefaRPServer.Services.Inventory
 {
     public class ItemFactory
     {
-        public BaseItem CreateClothItem(string name, EquipmentSlot slot, int drawableId, int textureId, int paletteId, bool isProp = false, string model = "p_cs_shirt_01_s")
+        public BaseItem CreateClothItem(string name, EquipmentSlot slot, int drawableId, int textureId, int paletteId, bool isProp = false, string model = "prop_ld_tshirt_02")
         {
             return new ClothItem(name, slot, drawableId, textureId, paletteId, isProp, model);
         }
@@ -13,6 +14,10 @@ namespace AltVStrefaRPServer.Services.Inventory
         public BaseItem CreateBurger(int stackSize = 20, ushort hungerValue = 20) => new FoodItem("Burger", stackSize, "prop_cs_burger_01", hungerValue);
         public BaseItem CreateWater(int stackSize = 10, ushort thirstValue = 10) => new FoodItem("Woda", stackSize, "ng_proc_brkbottle_02a", thirstValue);
         public BaseItem CreateSprunk(int stackSize = 10, ushort thirstvalue = 13) => new FoodItem("Sprunk", stackSize, "ng_proc_sodacan_01b", thirstvalue);
+
+        public WeaponItem CreatePistol(int ammo = 20) => new WeaponItem("Pistolet", "w_pi_pistol", WeaponModel.Pistol, ammo, EquipmentSlot.LeftHand);
+        public WeaponItem CreateCombatPistol(int ammo = 40) => new WeaponItem("Pistolet bojowy", "w_pi_combatpistol", WeaponModel.CombatPistol, ammo, EquipmentSlot.LeftHand);
+        public WeaponItem CreatePumpShotgun(int ammo = 8) => new WeaponItem("Strzelba", "w_sg_pumpshotgun", WeaponModel.PumpShotgun, ammo, EquipmentSlot.LeftHand);
 
         public BaseItem CreateItem(ItemType item)
         {
@@ -24,6 +29,12 @@ namespace AltVStrefaRPServer.Services.Inventory
                     return CreateBurger();
                 case ItemType.FoodItem_Water:
                     return CreateWater();
+                case ItemType.Weapon_CombatPistol:
+                    return CreateCombatPistol();
+                case ItemType.Weapon_Pistol:
+                    return CreatePistol();
+                case ItemType.Weapon_PumpShotgun:
+                    return CreatePumpShotgun();
                 default:
                     return null;
             }
