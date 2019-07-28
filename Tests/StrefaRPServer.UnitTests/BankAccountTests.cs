ff0216242a98839ee92b407b0f9382fdeef17926
PezeM <pezemos@gmail.com>
2019-07-28 23:33:19 +0200

    Przykladowe testy

---
+++ Tests/StrefaRPServer.UnitTests/BankAccountTests.cs
@@ -0,0 +1,25 @@
+﻿using AltVStrefaRPServer.Models;
+using NUnit.Framework;
+
+namespace StrefaRPServer.UnitTests
+{
+    public class BankAccountTests
+    {
+        private BankAccount _bankAccount;
+
+        [SetUp]
+        public void Setup()
+        {
+            _bankAccount = new BankAccount();
+        }
+
+        [Test]
+        public void BasicTest()
+        {
+            var moneyToDeposit = 20;
+            _bankAccount.DepositMoney(moneyToDeposit);
+            
+            Assert.AreEqual(_bankAccount.Money, moneyToDeposit);
+        }
+    }
+}
--- Tests/StrefaRPServer.UnitTests/UnitTest1.cs
+++ Tests/StrefaRPServer.UnitTests/UnitTest1.cs
@@ -1,12 +1,18 @@
+using System;
+using AltV.Net.Elements.Entities;
+using AltV.Net.Elements.Factories;
 using NUnit.Framework;
+using AltV.Net.Mock;
 
-namespace Tests
+namespace StrefaRPServer.UnitTests
 {
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
+            var mock = new MockBaseEntityPool(new MockPlayerPool(new PlayerFactory()), new MockVehiclePool(new VehicleFactory()));
+            mock.GetOrCreate(IntPtr.Zero, BaseObjectType.Player, out IEntity player);
         }
 
         [Test]
