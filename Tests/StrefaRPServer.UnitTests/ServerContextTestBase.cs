e0f1df1611deb72aa00b128f6f2d7ca153b4a1f4
PezeM <pezemos@gmail.com>
2019-07-29 13:18:11 +0200

    Bazowa klasa do testow db

---
+++ Tests/StrefaRPServer.UnitTests/ServerContextTestBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AltVStrefaRPServer.Database;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+
+namespace StrefaRPServer.UnitTests
+{
+    [TestFixture]
+    public abstract class ServerContextTestBase
+    {
+        protected ServerContext _context;
+        protected Mock<Func<ServerContext>> _mockFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<ServerContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            _context = new ServerContext(options);
+            _context.Database.EnsureCreated();
+
+            _mockFactory = new Mock<Func<ServerContext>>();
+            _mockFactory.Setup(f => f()).Returns(_context);
+        }
+    }
+}
