8eae30f775405178b3daaaa72273406d850e273b
PezeM <16854655+PezeM@users.noreply.github.com>
2019-08-30 01:42:01 +0200

    Zaktualizowano baze danych / nuget

---
+++ AltVStrefaRPServer/Database/Migrations/20190829233303_UpdatedHousesPriceType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AltVStrefaRPServer.Database.Migrations
+{
+    public partial class UpdatedHousesPriceType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Houses",
+                nullable: false,
+                oldClrType: typeof(float));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Houses",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
