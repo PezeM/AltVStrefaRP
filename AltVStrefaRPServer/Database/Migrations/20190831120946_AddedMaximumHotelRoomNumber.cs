b17af04747a941eee9dd65d19eca279132d15cc3
PezeM <16854655+PezeM@users.noreply.github.com>
2019-08-31 14:11:24 +0200

    Aktualizacja do net core 3.0

---
+++ AltVStrefaRPServer/Database/Migrations/20190831120946_AddedMaximumHotelRoomNumber.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AltVStrefaRPServer.Database.Migrations
+{
+    public partial class AddedMaximumHotelRoomNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaximumNumberOfRooms",
+                table: "HouseBuildings",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaximumNumberOfRooms",
+                table: "HouseBuildings");
+        }
+    }
+}
